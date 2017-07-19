3cdf38d2cfea91583330bf5a44263428c483a15f
Lucas Emmanoel Mascarenhas e Silva <lucas.mascarenhas@netservicos.com.br>
2017-07-19 12:02:12 -0300

    Adding comments'

--- chap_02/iterating_through_a_massive_file.php
+++ chap_02/iterating_through_a_massive_file.php
@@ -1,12 +1,20 @@
 <?php
+// taking advantage of AST
+// you need to run this using PHP 7
 
 define('MASSIVE_FILE', '/../data/files/war_and_peace.txt');
+
+// setup class autoloading
 require __DIR__ . '/../../Application/Autoload/Loader.php';
+
+// add current directory to the path
 Application\Autoload\Loader::init(__DIR__ . '/../..');
 
+// get iterator class
 try {
     $largeFile = new Application\Iterator\LargeFile(__DIR__ . MASSIVE_FILE);
     $iterator = $largeFile->getIterator('ByLine');
+    // NOTE: this comes back as an isntace of Generator, which implements Iterator
     $words = 0;
     foreach ($iterator as $line) {
         echo $line;
