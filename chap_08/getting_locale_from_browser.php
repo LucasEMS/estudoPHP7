2cb23b0c08454ec28952c99270e85fccac59fda5
Lucas Mascarenhas <lmskyline@hotmail.com>
2017-08-16 13:35:08 -0300

    Getting the locale from browser data

---
+++ chap_08/getting_locale_from_browser.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../../Application/Autoload/Loader.php';
+Application\Autoload\Loader::init(__DIR__ . '/../..');
+use Application\I18n\Locale;
+
+$locale = [NULL, 'fr-FR', 'da, en-gb;q=0.8, en;q=0.7'];
+
+?>
+<!DOCTYPE html>
+<head>
+	<title>PHP 7 Cookbook</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="php7cookbook_html_table.css">
+</head>
+<body>
+<table style="width:60%">
+<th>Accept-Language</th><th>Derived Locale</th></tr>
+<?php
+foreach ($locale as $code) {
+	$locale = new Locale($code); 
+	echo '<tr><td>' . htmlspecialchars($code) . '</td><td>' . $locale->getLocaleCode() . '</td></tr>';
+}
+?>
+</table>
+</body>
+</html>
