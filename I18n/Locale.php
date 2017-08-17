730fa96189c546241440a4431427b2ba619c4a95
Lucas Mascarenhas <lmskyline@hotmail.com>
2017-08-17 09:13:33 -0300

    Handling currency by locale.

--- I18n/Locale.php
+++ I18n/Locale.php
@@ -7,9 +7,67 @@ use NumberFormatter;
 class Locale extends PhpLocale
 {
     const FALLBACK_LOCALE = 'en';
+    const ERROR_UNABLE_TO_PARSE = 'ERROR: Unable to parse';
+    const FALLBACK_CURRENCY = 'GBP';
     
+    protected $currencyFormatter;
+    protected $currentyLookup;
+    protected $currencyCode;
     protected $numberFormatter;
     protected $localeCode;
+    
+    public function __construct($localeString = NULL,
+            IsoCodesInterface $currencyLookup = NULL)
+    {
+        if ($localeString) {
+            $this->setLocaleCode($localeString);
+        } else {
+            $this->setLocaleCode($this->getAcceptLanguage());
+        }
+        
+        $this->currencyLookup = $currencyLookup;
+        if ($this->currencyLookup) {
+            $this->currencyCode = $this->currencyLookup
+                            ->getCurrencyCodeFromIso2CountryCode($this
+                            ->getCountryCode())
+                            ->currency_code;
+        } else {
+            $this->currencyCode = self::FALLBACK_CURRENCY;
+        }
+    }
+    
+    public function formatCurrency($currency)
+    {
+        return $this->getCurrencyFormatter()
+                ->formatCurrency($currency, $this->currencyCode);
+    }
+    
+    public function parseCurrency($string)
+    {
+        $result = $this->getCurrencyFormatter()
+                ->parseCurrency($string, $this->currencyCode);
+        return ($result) ? $result : self::ERROR_UNABLE_TO_PARSE;
+    }
+            
+    public function getCountryCode()
+    {
+        return $this->getRegion($this->getLocalecode());
+    }
+    
+    public function getCurrencyCode()
+    {
+        return $this->currencyCode;
+    }
+    
+    public function getCurrencyFormatter()
+    {
+        if (!$this->currencyFormatter) {
+            $this->currencyFormatter =
+                    new NumberFormatter($this->getLocaleCode(),
+                            NumberFormatter::CURRENCY);
+        }
+        return $this->currencyFormatter;
+    }
     
     public function getNumberFormatter()
     {
@@ -49,12 +107,4 @@ class Locale extends PhpLocale
         return $this->localeCode;
     }
     
-    public function __construct($localeString = NULL)
-    {
-        if ($localeString) {
-            $this->setLocaleCode($localeString);
-        } else {
-            $this->setLocaleCode($this->getAcceptLanguage());
-        }
-    }
 }
