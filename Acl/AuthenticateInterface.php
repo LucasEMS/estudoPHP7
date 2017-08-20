331d2c8ec87dce6d4b09a5f1e746b80c72cf1d34
Lucas Mascarenhas <lmskyline@hotmail.com>
2017-08-20 12:16:52 -0300

    Using middleware to implement acess control

---
+++ Acl/AuthenticateInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Application\Acl;
+
+use Psr\Http\Message\ { RequestInterface, ResponseInterface };
+
+interface AuthenticateInterface
+{
+
+    /**
+     * @param RequestInterface $request
+     * @return ResponseInterface $response = where body = database info (if successful)
+     *         If not successful, returns status code = 400
+     */
+    public function login(RequestInterface $request) : 
+        ResponseInterface;
+}
