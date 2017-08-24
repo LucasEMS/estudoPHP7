ee0f5aaa9935475c63f512baf8194850b855b758
Lucas Mascarenhas <lmskyline@hotmail.com>
2017-08-24 12:32:46 -0300

    Implementing a Stack

---
+++ chap_10/stack.php
@@ -0,0 +1,23 @@
+<?php
+// setup class autoloading
+require __DIR__ . '/../../Application/Autoload/Loader.php';
+Application\Autoload\Loader::init(__DIR__ . '/../..');
+use Application\Generic\Stack;
+$stack = new Stack();
+
+echo 'Do Something .. ' . PHP_EOL;
+$stack->push('1st Message: ' . date('H:i:s'));
+sleep(3);
+
+echo 'Do Something Else ... ' . PHP_EOL;
+$stack->push('2nd Message: ' . date('H:i:s'));
+sleep(3);
+
+echo 'Do Something Else Again ... ' . PHP_EOL;
+$stack->push('3rd Message: ' . date('H:i:s'));
+sleep(3);
+
+echo 'What Time Is It?' . PHP_EOL;
+foreach ($stack() as $item) {
+    echo $item . PHP_EOL;
+}
