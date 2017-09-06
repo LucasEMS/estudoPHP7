98a67ab8aa9e8290ae913a3bd598c41777d236d8
Lucas Mascarenhas <lmskyline@hotmail.com>
2017-09-06 12:08:55 -0300

    Using Traits and Interfaces

---
+++ chap_13/trait_and_interface.php.php
@@ -0,0 +1,35 @@
+<?php
+// demonstrates classes which use traits
+
+define('DB_CONFIG_FILE','/../data/config/db.config.php');
+
+// setup class autoloading
+require __DIR__ . '/../../Application/Autoload/Loader.php';
+
+// add current directory to the path
+Application\Autoload\Loader::init(__DIR__ . '/../..');
+
+// get db params
+$params = include __DIR__ . DB_CONFIG_FILE;
+
+
+try {
+    // build list country class
+    echo "\n------------\n";
+    echo "Country List\n";
+    echo "------------\n";
+    $list = Application\Generic\ListFactory::factory(
+        new Application\Generic\CountryListUsingTrait(), $params);
+    foreach ($list->list() as $item) echo $item . ' ';
+    echo "\n\n\n\n";
+    
+    echo "\n------------\n";
+    echo "Customer List\n";
+    echo "-------------\n";
+    $list = Application\Generic\ListFactory::factory(
+            new Application\Generic\CustomerListUsingTrait(), $params);
+    foreach ($list->list() as $item) echo $item . ' ';
+} catch (Throwable $e) {
+    echo $e->getMessage();
+}
+
